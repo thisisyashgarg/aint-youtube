a22ba015c4bffb50fa5b1e7bebd1e119f580f178
Yash Garg <thisisyashgarg@gmail.com>
2023-03-12 16:16:21 +0530

    video details fetched

--- src/components/WatchPage.tsx
+++ src/components/WatchPage.tsx
@@ -2,12 +2,17 @@ import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/AppSLice";
 import { useSearchParams } from "react-router-dom";
-import { GET_COMMENTS_URL, RELATED_VIDEOS_URL } from "../utils/constants";
+import {
+  GET_COMMENTS_URL,
+  RELATED_VIDEOS_URL,
+  VIDEO_DETAILS_FROM_ID,
+} from "../utils/constants";
 import { getRelatedVideos } from "../utils/helper";
 import Comments from "./Comments";
 import { CommentsData } from "./Comments";
 import { getComments } from "../utils/helper";
 import RelatedVideos from "./RelatedVideos";
+import { VideoData } from "./VideoContainer";
 
 export type RelatedVideosType = {
   snippet: {
@@ -27,7 +32,7 @@ export type RelatedVideosType = {
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
-  // const [videoDetails, setVideoDetails] = useState({});
+  const [videoDetails, setVideoDetails] = useState<VideoData>();
   const [comments, setComments] = useState<Array<CommentsData>>([]);
   const [relatedVideos, setRelatedVideos] = useState<Array<RelatedVideosType>>(
     []
@@ -36,19 +41,20 @@ const WatchPage = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    console.log("use effect ran");
     dispatch(closeMenu());
-    // getVideoDetailsFromID();
+    getVideoDetailsFromID();
     getComments(setComments, GET_COMMENTS_URL, videoID);
     getRelatedVideos(setRelatedVideos, RELATED_VIDEOS_URL, videoID);
   }, [videoID]);
 
-  // console.log(videoDetails?.snippet);
+  console.log(videoDetails);
 
-  // async function getVideoDetailsFromID() {
-  //   const data = await fetch(VIDEO_DETAILS_FROM_ID + videoID);
-  //   const json = await data.json();
-  //   setVideoDetails(json.items[0]);
-  // }
+  async function getVideoDetailsFromID() {
+    const data = await fetch(VIDEO_DETAILS_FROM_ID + videoID);
+    const json = await data.json();
+    setVideoDetails(json.items[0]);
+  }
 
   return (
     <div className="flex mt-20">
@@ -62,10 +68,14 @@ const WatchPage = () => {
           allowFullScreen
         ></iframe>
 
-        <div className="w-[100%]">
-          {/* <h1 className="text-3xl font-semibold">{title}</h1> */}
-          {/* <img src={videoDetails[0].snippet} /> */}
-          {/* <p className="text-xl">{channelTitle}</p> */}
+        <div className="w-[100%] mt-3">
+          <h1 className="text-3xl font-semibold">
+            {videoDetails?.snippet?.title}
+          </h1>
+          <p className="text-xl">{videoDetails?.snippet?.channelTitle}</p>
+          <p className="text-m text-gray-500">
+            {videoDetails?.statistics?.viewCount} views
+          </p>
         </div>
 
         <div className="m-2 w-[600px] space-y-5 mt-6">
