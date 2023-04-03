d7fa95dbbc86f4177c2026e529536c362b4df40b
Yash Garg <thisisyashgarg@gmail.com>
2023-04-03 16:18:53 +0530

    applied debouncing, added api key to .env

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { toggleMenu } from "../utils/AppSLice";
+import searchLogo from "../search.svg";
 import {
   HAMBURGER_LOGO_URL,
   YOUTUBE_LOGO_URL,
@@ -13,21 +14,23 @@ const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState<Array<VideoData>>([]);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     getSearchsuggestions();
-  //   }, 300);
-  //   return () => {
-  //     clearTimeout(timer);
-  //   };
-  // }, [searchQuery]);
-
   console.log(searchResults);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      getSearchSuggestions();
+    }, 300);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [searchQuery]);
 
   async function getSearchSuggestions() {
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
     setSearchResults(json.items);
+    // console.log(searchResults);
   }
 
   function toggleSidebar() {
@@ -35,40 +38,52 @@ const Header = () => {
   }
 
   return (
-    <div className="flex justify-between bg-white z-10 items-center shadow-lg h-16 px-4 fixed top-0 left-0 right-0 ">
-      <div className="flex items-center p-2 ">
+    <div className="flex flex-col items-center ">
+      <div className="flex justify-between bg-white z-10 items-center shadow-lg h-16 px-4 fixed top-0 left-0 right-0 ">
+        <div className="flex items-center p-2 ">
+          <img
+            alt="hamburger"
+            onClick={() => toggleSidebar()}
+            className=" h-10 cursor-pointer"
+            src={HAMBURGER_LOGO_URL}
+          />
+          <a href="/">
+            <img alt="icon" className="h-20" src={YOUTUBE_LOGO_URL} />
+          </a>
+        </div>
+
+        <div className="flex mx-6 items-center">
+          <input
+            placeholder="Search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            type="text"
+            className="rounded-l-full border  border-gray-400 p-2 w-96 px-5"
+          />
+          <button
+            className="rounded-r-full border  border-gray-400 p-2"
+            onClick={getSearchSuggestions}
+          >
+            Search
+          </button>
+        </div>
+
         <img
-          alt="hamburger"
-          onClick={() => toggleSidebar()}
-          className=" h-10 cursor-pointer"
-          src={HAMBURGER_LOGO_URL}
+          alt="user"
+          className="w-10 h-10 items-center rounded-full"
+          src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
         />
-        <a href="/">
-          <img alt="icon" className="h-20" src={YOUTUBE_LOGO_URL} />
-        </a>
       </div>
-
-      <div className="flex mx-6 items-center">
-        <input
-          placeholder="Search"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          type="text"
-          className="rounded-l-full border  border-gray-400 p-2 w-96 px-5"
-        />
-        <button
-          className="rounded-r-full border  border-gray-400 p-2"
-          onClick={getSearchSuggestions}
-        >
-          Search
-        </button>
+      <div className="mt-20 fixed bg-white px-4 py-4 w-80 rounded-md border border-gray-100 shadow-lg ">
+        <ul className="space-y-3">
+          {/* {searchResults.map((video) => (
+            <li className="p-1 space-x-3 m-1 flex">
+              <img src={searchLogo} alt="logo" className="w-6 " />{" "}
+              <span>{video.snippet.title}</span>
+            </li>
+          ))} */}
+        </ul>
       </div>
-
-      <img
-        alt="user"
-        className="w-10 h-10 items-center rounded-full"
-        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
-      />
     </div>
   );
 };
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,10 @@
-export const YOUTUBE_API_URL = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=AIzaSyCfxDLscmoV3eg0fXxDYkGR8tmq_RHehZM`;
+export const YOUTUBE_API_URL = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`;
 export const HAMBURGER_LOGO_URL =
   "https://cdn-icons-png.flaticon.com/512/6065/6065135.png";
 export const YOUTUBE_LOGO_URL =
   "https://www.logo.wine/a/logo/YouTube/YouTube-Logo.wine.svg";
-export const RELATED_VIDEOS_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=30&key=AIzaSyCfxDLscmoV3eg0fXxDYkGR8tmq_RHehZM&relatedToVideoId=`;
-export const GET_COMMENTS_URL =
-  "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=35&key=AIzaSyCfxDLscmoV3eg0fXxDYkGR8tmq_RHehZM&videoId=";
-export const YOUTUBE_SEARCH_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&key=AIzaSyCfxDLscmoV3eg0fXxDYkGR8tmq_RHehZM&q=`;
+export const RELATED_VIDEOS_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=30&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&relatedToVideoId=`;
+export const GET_COMMENTS_URL = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=35&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&videoId=`;
+export const YOUTUBE_SEARCH_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&q=`;
 export const CORS_HEROKU_PROXY = "https://cors-anywhere.herokuapp.com/";
-export const VIDEO_DETAILS_FROM_ID =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=AIzaSyCfxDLscmoV3eg0fXxDYkGR8tmq_RHehZM&id=";
+export const VIDEO_DETAILS_FROM_ID = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&id=`;
